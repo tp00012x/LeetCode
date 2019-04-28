1ca07a66ab203bd9894ce4de4837a41c11a5c0f1
Anthony Torres <anthony_torres01@yahoo.com>
2019-04-28 11:54:14 -0500

    Add another solution

--- first_unique_character.py
+++ first_unique_character.py
@@ -1,13 +1,24 @@
 from collections import Counter
 
-def firstUniqChar(s: str) -> int:
-    count = Counter(s)
-    print(count)
-    for i, ch in enumerate(s):
-        if count[ch] == 1:
-            return i 
-    return -1
+# def firstUniqChar(s: str) -> int:
+#     count = Counter(s)
+#     for i, ch in enumerate(s):
+#         if count[ch] == 1:
+#             return i 
+#     return -1
 
+# Runtime: 120 ms, faster than 63.38%
 
-print(firstUniqChar("loveleetcode"))
+class Solution:
+    def firstUniqChar(self, s: str):
+        count = Counter(s)
+        
+        index = 0
+        for ch in s:
+            if count[ch] == 1:
+                return index
+            else:
+                index += 1       
+        return -1
 
+# Runtime: 140 ms, faster than 42.15% 
